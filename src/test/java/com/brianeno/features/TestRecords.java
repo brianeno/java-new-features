47b571f093b8c0ab86325c4da0815b9c5d469475
agus-mac-031 <benochson@arisglobal.com>
2023-09-13 17:27:25 -0400

    initial checkin

---
+++ src/test/java/com/brianeno/features/TestRecords.java
@@ -0,0 +1,11 @@
+package com.brianeno.features;
+
+import org.junit.jupiter.api.Test;
+
+public class TestRecords {
+
+    @Test
+    public void testRecord() {
+        System.out.println("Test");
+    }
+}
